164cf9593b56a7903a97886f934dd3767452f63e
manojpannala <manojpannala@outlook.com>
2021-06-10 16:28:56 +0300

    Minor changes with Descriptions

--- module/vpc/variables.tf
+++ module/vpc/variables.tf
@@ -1,24 +1,40 @@
+variable "AWS_REGION" {
+    default = "eu-central-1"
+    type = string
+}
+
 variable "VPC_CIDR_BLOCK" {
     default = "10.0.0.0/16"
+    type = string
+    description = "The CIDR block for VPC"
 }
 
 variable "ENVIRONMENT" {
     default = ""
     type = string
+    description = "VPC ENVIRONMENT"
 }
 
 variable "VPC_PUBLIC_SUBNET_1_CIDR" {
-    default = "10.0.1.0/24"
+    default = "10.0.101.0/24"
+    type = string
+    description = "The CIDR block for VPC"
 }
 
 variable "VPC_PUBLIC_SUBNET_2_CIDR" {
-    default = "10.0.2.0/24"
+    default = "10.0.102.0/24"
+    type = string
+    description = "The CIDR block for VPC"
 }
 
 variable "VPC_PRIVATE_SUBNET_1_CIDR" {
-    default = "10.0.3.0/24"
+    default = "10.0.1.0/24"
+    type = string
+    description = "The CIDR block for VPC"
 }
 
 variable "VPC_PRIVATE_SUBNET_2_CIDR" {
-    default = "10.0.4.0/24"
+    default = "10.0.2.0/24"
+    type = string
+    description = "The CIDR block for VPC"
 }
