cd92576484839c6478e0e004c5500ba7e466abd3
manojpannala <manojpannala@outlook.com>
2021-06-10 17:26:25 +0300

    Define Security Group for the App

--- module/app/app_instance.tf
+++ module/app/app_instance.tf
@@ -10,3 +10,38 @@ module "mrp_rds" {
     AWS_REGION = var.AWS_REGION
 }
 
+# Define Security Group for the App
+resource "aws_security_group" "mrp_app" {
+    tags = {
+        Name = "${var.ENVIRONMENT}-mrp-app"
+    }
+
+    name = "${var.ENVIRONMENT}-mrp-app"
+    description = "Created by MRP"
+    vpc_id = module.mrp_vpc.my_vpc_id
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["${var.SSH_CIDR_APP}"]
+    }
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
