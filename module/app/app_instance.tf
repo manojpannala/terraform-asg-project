c2f4cda06f19e307deaa75d8f64111bb0229e37b
manojpannala <manojpannala@outlook.com>
2021-06-10 17:20:48 +0300

    added VPC and RDS modules in the app_instance

--- module/app/app_instance.tf
+++ module/app/app_instance.tf
@@ -1,4 +1,12 @@
 module "mrp_vpc" {
     source = "../module/vpc"
-        
+    ENVIRONMENT = var.ENVIRONMENT
+    AWS_REGION = var.AWS_REGION
 }
+
+module "mrp_rds" {
+    source = "../module/rds"
+    ENVIRONMENT = var.ENVIRONMENT
+    AWS_REGION = var.AWS_REGION
+}
+
