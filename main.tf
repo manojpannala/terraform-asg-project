e5d9121dc7ccfab66ee831efdb551e43ea26ab6a
manojpannala <manojpannala@outlook.com>
2021-06-10 21:37:03 +0300

    fix main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,27 @@
+provider "aws" {
+    region = var.AWS_REGION  
+}
+
 module "mrp_vpc" {
-    source = "./app"  
+    source = "./module/vpc"  
+
     ENVIRONMENT = var.ENVIRONMENT
     AWS_REGION = var.AWS_REGION
 }
 
-provider "aws" {
-    region = var.AWS_REGION  
+module "mrp_app" {
+    source = "./app"
+    
+    ENVIRONMENT = var.ENVIRONMENT
+    AWS_REGION  = var.AWS_REGION
+    vpc_private_subnet1 = module.mrp-vpc.mrp_vpc_private_subnet_1
+    vpc_private_subnet2 = module.mrp-vpc.mrp_vpc_private_subnet_2
+    vpc_id = module.mrp-vpc.my_vpc_id
+    vpc_public_subnet1 = module.mrp-vpc.mrp_vpc_public_subnet_1
+    vpc_public_subnet2 = module.mrp-vpc.mrp_vpc_public_subnet_2
+}
+
+output "load_balancer_output" {
+  description = "Load Balancer"
+  value       = module.mrp-app.load_balancer_output
 }
--- variables.tf
+++ variables.tf
@@ -8,3 +8,15 @@ variable "ENVIRONMENT" {
     type = string
     default = "development"
 }
+
+variable "INSTANCE_TYPE" {
+    default = "t2.micro" 
+}
+
+variable "AMIS" {
+    type = map
+    default = {
+        eu-central-1 = "ami-0980c5102b5ef10cc",
+        eu-west-1 = "ami-06c5b2809791cf59c"
+    }
+}
