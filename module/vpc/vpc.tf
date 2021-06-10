31edc55a98dddff75454b1403f6294c5d5624d40
manojpannala <manojpannala@outlook.com>
2021-06-10 09:26:22 +0300

    Added Public Subnets

--- module/vpc/vpc.tf
+++ module/vpc/vpc.tf
@@ -12,3 +12,29 @@ resource "aws_vpc" "mrp_vpc" {
         Name = "${var.ENVIRONMENT}-vpc"
     }
 }
+
+# Public Subnets
+
+# Public-Subnet-1
+resource "aws_subnet" "mrp_vpc_public_subnet_1" {
+    vpc_id = aws_vpc.mrp_vpc.id  
+    cidr_block = var.VPC_PUBLIC_SUBNET_1_CIDR
+    availability_zone = data.aws_availability_zones.available.names[0]
+    map_public_ip_on_launch = true
+    
+    tags = {
+        Name = "${var.ENVIRONMENT}-mrp_vpc_public_subnet_1"
+    }
+}
+
+# Public-Subnet-2
+resource "aws_subnet" "mrp_vpc_public_subnet_2" {
+    vpc_id = aws_vpc.mrp_vpc.id  
+    cidr_block = var.VPC_PUBLIC_SUBNET_2_CIDR
+    availability_zone = data.aws_availability_zones.available.names[0]
+    map_public_ip_on_launch = true
+    
+    tags = {
+        Name = "${var.ENVIRONMENT}-mrp_vpc_public_subnet_2"
+    }
+}
