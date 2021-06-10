76bd261b6488ea87156b0c0aac972189f5163f61
manojpannala <manojpannala@outlook.com>
2021-06-10 09:38:23 +0300

    Added Private Subnets

--- module/vpc/vpc.tf
+++ module/vpc/vpc.tf
@@ -38,3 +38,29 @@ resource "aws_subnet" "mrp_vpc_public_subnet_2" {
         Name = "${var.ENVIRONMENT}-mrp_vpc_public_subnet_2"
     }
 }
+
+# Private Subnets
+
+# Private-Subnet-1
+resource "aws_subnet" "mrp_vpc_private_subnet_1" {
+    vpc_id = aws_vpc.mrp_vpc.id  
+    cidr_block = var.VPC_PRIVATE_SUBNET_1_CIDR
+    availability_zone = data.aws_availability_zones.available.names[0]
+    # map_public_ip_on_launch = true
+    
+    tags = {
+        Name = "${var.ENVIRONMENT}-mrp_vpc_private_subnet_1"
+    }
+}
+
+# Private-Subnet-2
+resource "aws_subnet" "mrp_vpc_private_subnet_2" {
+    vpc_id = aws_vpc.mrp_vpc.id  
+    cidr_block = var.VPC_PRIVATE_SUBNET_2_CIDR
+    availability_zone = data.aws_availability_zones.available.names[0]
+    # map_public_ip_on_launch = true
+    
+    tags = {
+        Name = "${var.ENVIRONMENT}-mrp_vpc_private_subnet_2"
+    }
+}
