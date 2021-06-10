b87a2e35c649faa8062d1cca15af3157c0d2148c
manojpannala <manojpannala@outlook.com>
2021-06-10 21:39:21 +0300

    test error

--- main.tf
+++ main.tf
@@ -14,14 +14,14 @@ module "mrp_app" {
     
     ENVIRONMENT = var.ENVIRONMENT
     AWS_REGION  = var.AWS_REGION
-    vpc_private_subnet1 = module.mrp-vpc.mrp_vpc_private_subnet_1
-    vpc_private_subnet2 = module.mrp-vpc.mrp_vpc_private_subnet_2
-    vpc_id = module.mrp-vpc.my_vpc_id
-    vpc_public_subnet1 = module.mrp-vpc.mrp_vpc_public_subnet_1
-    vpc_public_subnet2 = module.mrp-vpc.mrp_vpc_public_subnet_2
+    vpc_private_subnet1 = module.mrp_vpc.mrp_vpc_private_subnet_1
+    vpc_private_subnet2 = module.mrp_vpc.mrp_vpc_private_subnet_2
+    vpc_id = module.mrp_vpc.my_vpc_id
+    vpc_public_subnet1 = module.mrp_vpc.mrp_vpc_public_subnet_1
+    vpc_public_subnet2 = module.mrp_vpc.mrp_vpc_public_subnet_2
 }
 
 output "load_balancer_output" {
   description = "Load Balancer"
-  value       = module.mrp-app.load_balancer_output
+  value       = module.mrp_app.load_balancer_output
 }
