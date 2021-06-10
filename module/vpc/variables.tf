b3254eca402c693d56ea5563b010dcf3fbc1700b
manojpannala <manojpannala@outlook.com>
2021-06-10 09:27:39 +0300

    Public subnets vars

--- module/vpc/variables.tf
+++ module/vpc/variables.tf
@@ -4,4 +4,13 @@ variable "VPC_CIDR_BLOCK" {
 
 variable "ENVIRONMENT" {
     default = ""
+    type = string
+}
+
+variable "VPC_PUBLIC_SUBNET_1_CIDR" {
+    default = "10.0.1.0/24"
+}
+
+variable "VPC_PUBLIC_SUBNET_2_CIDR" {
+    default = "10.0.2.0/24"
 }
