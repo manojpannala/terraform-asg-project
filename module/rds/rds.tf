6b873858f9866944f0c33a9905fc5cafd9c9a955
manojpannala <manojpannala@outlook.com>
2021-06-10 20:19:58 +0300

    remove vpc module

--- module/rds/rds.tf
+++ module/rds/rds.tf
@@ -1,17 +1,17 @@
 # Call VPC module first to get the Subnet IDs
-module "mrp_vpc" {
-    source = "../vpc"  
-    ENVIRONMENT = var.ENVIRONMENT
-    AWS_REGION = var.AWS_REGION
-}
+# module "mrp_vpc" {
+#     source = "../vpc"  
+#     ENVIRONMENT = var.ENVIRONMENT
+#     AWS_REGION = var.AWS_REGION
+# }
 
 # Define Subnet Group for RDS Service
 resource "aws_db_subnet_group" "mrp-rds-subnet-group" {
     name = "${var.ENVIRONMENT}-mrp-rds-db"  
     description = "Allowed subnets for DB cluster instances"
     subnet_ids = [ 
-        "${module.private_subnet_1_id}",
-        "${module.private_subnet_2_id}" 
+        "${module.vpc_private_subnet1}",
+        "${module.vpc_private_subnet2}" 
     ]
     tags = {
         Name = "${var.ENVIRONMENT}-mrp-db-subnet"
