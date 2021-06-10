ec355241181675a61343f2d30e705b344e2349fe
manojpannala <manojpannala@outlook.com>
2021-06-10 16:15:58 +0300

    Route Table Association with Public & Private Subnets

--- module/vpc/vpc.tf
+++ module/vpc/vpc.tf
@@ -121,3 +121,14 @@ resource "aws_route_table" "private-rtb" {
     }
 }
 
+# Route Table Association with Public Subnets
+resource "aws_route_table_association" "to_public_subnets" {
+    subnet_id = aws_subnet.mrp_vpc_public_subnet_1.id
+    route_table_id = aws_route_table.public-rtb.id
+}
+
+# Route Table Association with Private Subnets
+resource "aws_route_table_association" "to_private_subnets" {
+    subnet_id = aws_subnet.mrp_vpc_private_subnet_1.id
+    route_table_id = aws_route_table.private-rtb.id
+}
