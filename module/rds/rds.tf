560bf0194ba51a3c8410cf35edd7cc4a42240939
manojpannala <manojpannala@outlook.com>
2021-06-10 16:38:02 +0300

    Define Subnet Group for RDS Service

--- module/rds/rds.tf
+++ module/rds/rds.tf
@@ -5,3 +5,16 @@ module "mrp-vpc" {
     AWS_REGION = var.AWS_REGION
 }
 
+# Define Subnet Group for RDS Service
+resource "aws_db_subnet_group" "mrp-rds-subnet-group" {
+    name = "${var.ENVIRONMENT}-mrp-rds-db"  
+    description = "Allowed subnets for DB cluster instances"
+    subnet_ids = [ 
+        "${module.mrp_vpc_private_subnet_1}",
+        "${module.mrp_vpc_private_subnet_2}" 
+    ]
+    tags = {
+        Name = "${var.ENVIRONMENT}-mrp-db-subnet"
+    }
+}
+
